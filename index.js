3fd2a8bbd1ee14d539b381044a04586f48920864
Shane Weaver <sgtw_speed@yahoo.com>
2015-01-08 19:48:44 +0000

    pushbullet notify, file renames

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-require('./lib/door.js').init({ relay: 0, opened: 0, closed: 1 });
+require('./lib/notify.pushbullet.js');  // Set with EXPORT PUSHBULLET=<your api key>
 
-require('./lib/doormonitor.js');
+require('./lib/door.control.js').init({ relay: 0, opened: 0, closed: 1 });
+
+require('./lib/door.monitor.js');
 
 require('./lib/server.js');
