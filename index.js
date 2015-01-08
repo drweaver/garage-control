e83bb51fabfa3b0e0dfdc7700da0ecb26a3a350a
Shane Weaver <sgtw_speed@yahoo.com>
2015-01-08 20:00:10 +0000

    more options to provide lat,lng

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ require('./lib/door.control.js').init({ relay: 0, opened: 0, closed: 1 });
 
 require('./lib/door.monitor.js');
 
+require('./lib/auth.pos.js').init({ distance: 3 }); // Set Lat,Lng with EXPORT LATLNG=<lat>,<lng> or in init({ distance: 3, lat: <lat>, lng: <lng>})
+
 require('./lib/server.js');
