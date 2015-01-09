f368eed6c117c0916f43ead857a06f399dc72c11
Shane Weaver <sgtw_speed@yahoo.com>
2015-01-09 16:49:47 +0000

    event bus used for getting door status and operating

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 //
 require('./lib/notify.pushbullet.js');  // Set with EXPORT PUSHBULLET=<your api key>
 
-require('./lib/door.control.js').init({ relay: 0, opened: 1, closed: 0 });
-
 require('./lib/door.monitor.js');
+
+require('./lib/door.control.js').init({ relay: 0, opened: 1, closed: 0 });
 
 require('./lib/auth.pos.js').init({ distance: 3 }); // Set Lat,Lng with EXPORT LATLNG=<lat>,<lng> or in init({ distance: 3, lat: <lat>, lng: <lng>})
 
