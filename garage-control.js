875d977b8f7bb805205b4327b5b3915144231b6e
Shane Weaver <sgtw_speed@yahoo.com>
2016-12-22 14:31:59 +0000

    first pass for amqp client

--- garage-control.js
+++ garage-control.js
@@ -6,13 +6,13 @@ var config = require('./config.json');
 
 require('./lib/notify.pushbullet.js').init(config.pushbullet);  // Set with EXPORT PUSHBULLET=<your api key>
 
+require('./lib/delay.event.js').init(config.delay);
+
 require('./lib/door.monitor.js').init(config.notify);
 
 require('./lib/door.control.js').init(config.pfio);
 
-require('./lib/mqtt.local.js').init(config.mqtt);
-
-//require('./lib/mqtt.remote.js').init(config.mqtt);
+require('./lib/amqp.js').init(config.amqp);
 
 require('./lib/auth.pos.js').init(config.pos); // Set Lat,Lng with EXPORT LATLNG=<lat>,<lng> or in init({ distance: 3, lat: <lat>, lng: <lng>})
 
